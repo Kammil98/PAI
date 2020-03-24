9e7d0a0b0af8c8929e0817993a31f2769720f9ad
Kammil98 <kamil.luwanski@gmail.com>
2020-03-24 10:14:10 +0100

    Done Ex3 - JavaScript

--- zad3/form_check.js
+++ zad3/form_check.js
@@ -52,15 +52,42 @@ function validate(formHandler) {
 
 function alterRows(i, e) {
 	 if (e) {
-		 if (i % 2 == 1) {
-			e.setAttribute("style", "background-color: Aqua;");
+		 e = e[0].getElementsByTagName("tr");   
+		 for(i = 0; i < e.length; i+=2) {
+			e[i].setAttribute("style", "background-color: Aqua;");
 		}
-		 e = e.nextSibling;
-		 while (e && e.nodeType != 1) {
-			e = e.nextSibling;
-		 }
-		 alterRows(++i, e);
 	 }
 }
+
+function nextNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.nextSibling;
+ }
+ return e;
+}
+
+function prevNode(e) {
+ while (e && e.nodeType != 1) {
+ e = e.previousSibling;
+ }
+ return e;
+}
+
+function swapRows(b) {
+ let tab = prevNode(b.previousSibling);
+ let tBody = nextNode(tab.firstChild);
+ let lastNode = prevNode(tBody.lastChild);
+ tBody.removeChild(lastNode);
+ let firstNode = nextNode(tBody.firstChild);
+ tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+ if (form.value.length > maxSize)
+ form.value = form.value.substring(0, maxSize);
+ else
+ msg.innerHTML = maxSize - form.value.length;
+}
+
 
 
